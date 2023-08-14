e3c6888b182ab16e8fbc92f9d264c310cc07eaf8
ThanhHaiPC <ThanhHaiPC2002@gmail.com>
2023-08-14 19:36:26 +0700

    Add project files.

---
+++ BaseMarket.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaseMarket.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
